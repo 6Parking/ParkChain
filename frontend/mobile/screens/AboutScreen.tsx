30a3f1cb1cfce25722e92f1ed2a2ae8830a70763
Myetekk <mirekmich19@gmail.com>
2026-04-19 09:10:57 +0200

    FAQ content

--- frontend/mobile/screens/AboutScreen.tsx
+++ frontend/mobile/screens/AboutScreen.tsx
@@ -9,8 +9,8 @@ export default function AboutScreen({ route, navigation }: any) {
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
-                <Text style={styles.welcome}>FAQ</Text>
-                <Text style={styles.welcome}>Rules</Text>
+                <Text style={styles.title}>FAQ</Text>
+                <Text style={styles.description}>ParkChain is a decentralized peer-to-peer parking marketplace. It empowers drivers to quickly find and rent available parking spaces in their desired location. It also provides a convenient platform for parking spot owners to list their unused spaces for both long- and short-term rental, allowing them to set a custom price and earn money effortlessly.</Text>
             </View>
         </SafeAreaView>
     );
@@ -19,7 +19,12 @@ export default function AboutScreen({ route, navigation }: any) {
 const styles = StyleSheet.create({
     container: { flex: 1, backgroundColor: '#fff' },
     content: { padding: 30, flex: 1 },
-    welcome: {
+    title: {
+        fontSize: 24,
+        fontWeight: "800",
+        color: "#1A1A1A",
+    },
+    description: {
         fontSize: 18,
         color: "#666",
         marginBottom: 30,
